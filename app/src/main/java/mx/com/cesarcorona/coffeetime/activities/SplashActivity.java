d7f588d1a9f81fce77c4a995d5ee957f2f67c2cb
Cesar Cabrera Corona <contacto@cesarcorona.ninja>
2017-08-15 01:28:33 -0500

    Add Splash Activity  Add Social Media Login : Facebook , Twitter , Google.  Add Firebase Configuration

--- app/src/main/java/mx/com/cesarcorona/coffeetime/activities/SplashActivity.java
+++ app/src/main/java/mx/com/cesarcorona/coffeetime/activities/SplashActivity.java
@@ -1,18 +1,135 @@
 package mx.com.cesarcorona.coffeetime.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import mx.com.cesarcorona.coffeetime.MainActivity;
 import mx.com.cesarcorona.coffeetime.R;
 
 
 public class SplashActivity extends AppCompatActivity {
 
+    public static final String TAG = SplashActivity.class.getSimpleName();
+    private static final long SPLASH_SCREEN_DELAY = 1000;
+    private final int RC_SIGN_IN = 100;
+    private static final boolean DEBUG = true;
+
+    private FirebaseAuth auth;
+    private LinearLayout rootView;
+
+
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
+        auth = FirebaseAuth.getInstance();
+        rootView = (LinearLayout) findViewById(R.id.root_view);
+
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Thread.currentThread()
+                        .setName(this.getClass().getSimpleName() + ": " + Thread.currentThread().getName());
+
+                if (auth.getCurrentUser() != null) {
+                    goToMainActivity();
+                } else {
+                    startSiginRegisterIntent();
+                }
+
+
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+
+
+
+    } // Fin onCreate()
+
+
+    private void goToMainActivity() {
+        Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
     }
-}
+
+    private void startSiginRegisterIntent(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                        new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                        new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build(),
+                                        new AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build()))
+                        .setTosUrl(getString(R.string.tos_url))
+                        .setPrivacyPolicyUrl(getString(R.string.tos_url))
+                        .setIsSmartLockEnabled(!DEBUG)
+                        .setTheme(R.style.AppTheme)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == ResultCodes.OK) {
+                goToMainActivity();
+                finish();
+                return;
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    showSnackbar(R.string.sign_in_cancelled);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackbar(R.string.unknown_error);
+                    return;
+                }
+            }
+
+            showSnackbar(R.string.unknown_error);
+        }
+    }
+
+    private void showSnackbar( int errorMessageRes) {
+        Snackbar.make(rootView, errorMessageRes, Snackbar.LENGTH_LONG).show();
+    }
+
+
+} // Fin SplashScreen.java
