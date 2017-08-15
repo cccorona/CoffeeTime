7a893b318d9dcd1c48c85c821db67979a73c1ddc
Cesar Cabrera Corona <contacto@cesarcorona.ninja>
2017-08-14 23:46:08 -0500

    Initial commit

---
+++ app/src/main/java/mx/com/cesarcorona/coffeetime/activities/SplashActivity.java
@@ -0,0 +1,18 @@
+package mx.com.cesarcorona.coffeetime.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import mx.com.cesarcorona.coffeetime.R;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
