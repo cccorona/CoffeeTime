8dd29e6920d2a5d1b93463779cabe249f989cb7e
ccoronacesar <itachi.upiita@gmail.com>
2017-10-06 01:09:19 -0500

    add all coffetyme path

--- app/src/main/java/mx/com/cesarcorona/coffeetime/fragment/CoffeOptionsFragment.java
+++ app/src/main/java/mx/com/cesarcorona/coffeetime/fragment/CoffeOptionsFragment.java
@@ -6,6 +6,7 @@ import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -22,6 +23,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -32,6 +35,7 @@ import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.appolica.flubber.Flubber;
 import com.google.android.gms.common.ConnectionResult;
@@ -53,8 +57,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -65,7 +73,10 @@ import java.util.Locale;
 import mx.com.cesarcorona.coffeetime.R;
 import mx.com.cesarcorona.coffeetime.activities.CoffeTimeActiviy;
 import mx.com.cesarcorona.coffeetime.activities.FilterActivity;
+import mx.com.cesarcorona.coffeetime.activities.FilterTopicsActivity;
+import mx.com.cesarcorona.coffeetime.activities.SearchActivity;
 import mx.com.cesarcorona.coffeetime.adapter.CategoryAdapter;
+import mx.com.cesarcorona.coffeetime.adapter.TopicsAdapter;
 import mx.com.cesarcorona.coffeetime.pojo.Categoria;
 import mx.com.cesarcorona.coffeetime.pojo.Topic;
 import mx.com.cesarcorona.coffeetime.pojo.User;
@@ -77,6 +88,13 @@ import noman.googleplaces.PlacesListener;
 
 import static android.content.Context.MODE_PRIVATE;
 import static android.view.View.GONE;
+import static mx.com.cesarcorona.coffeetime.activities.CoffeTimeActiviy.KEY_DATE;
+import static mx.com.cesarcorona.coffeetime.activities.CoffeTimeActiviy.KEY_TIME;
+import static mx.com.cesarcorona.coffeetime.activities.FilterActivity.KEY_CATEGORIA;
+import static mx.com.cesarcorona.coffeetime.activities.FilterActivity.KEY_CURRENT_LATIDU;
+import static mx.com.cesarcorona.coffeetime.activities.FilterActivity.KEY_CURRENT_LONGITUD;
+import static mx.com.cesarcorona.coffeetime.activities.FilterActivity.KEY_FAVORITE_PLACE;
+import static mx.com.cesarcorona.coffeetime.activities.FilterActivity.KEY_PLACE_SELECTED;
 import static mx.com.cesarcorona.coffeetime.services.MyFirebaseInstanceIDService.KEY_TOKEN;
 import static mx.com.cesarcorona.coffeetime.services.MyFirebaseInstanceIDService.PREFERENCES_KEY;
 import static mx.com.cesarcorona.coffeetime.services.MyFirebaseInstanceIDService.USERS_REFERENCE;
@@ -94,9 +112,12 @@ public class CoffeOptionsFragment extends Fragment implements OnMapReadyCallback
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 100;
     private static final int MAX_ZOOM = 16;
     private static final String CATEGORIAS_REFERENCE = "categorias";
+    private static final String TOPICS_REFERENCE = "topicos";
+
+
+    public static String KEY_DATE = "date";
+    public static String KEY_TIME = "time";
     public static final String KEY_PARTY_NUMBER = "party";
-
-
     public static String KEY_CATEGORIA="categoria";
     public static String KEY_FAVORITE_PLACE="place";
     public static String KEY_CURRENT_LATIDU="latitud";
@@ -137,7 +158,6 @@ public class CoffeOptionsFragment extends Fragment implements OnMapReadyCallback
     private ProgressDialog pDialog;
     private LinkedList<Categoria> allcategorias;
     private Spinner categoriasSelector;
-    private CategoryAdapter categoryAdapter;
     private Categoria categoriaSeleccionada;
     private SupportPlaceAutocompleteFragment autocompleteFragment;
     private Place ubicacionPreferida;
@@ -154,6 +174,17 @@ public class CoffeOptionsFragment extends Fragment implements OnMapReadyCallback
     private int number = 1;
     private ScrollView myScroolView;
     private boolean justCoffe;
+
+    private LinkedList<Topic> allTopics;
+    private Spinner topicSelector;
+    private TopicsAdapter categoryAdapter;
+    private RelativeLayout searchButtonR;
+
+    private String dateSelected , timeSelected;
+
+
+
+
 
 
 
@@ -183,6 +214,12 @@ public class CoffeOptionsFragment extends Fragment implements OnMapReadyCallback
         timeFinish = (ImageView) rootView.findViewById(R.id.date_finish);
         locationFinish = (ImageView)rootView.findViewById(R.id.location_finish);
         topiFinish = (ImageView)rootView.findViewById(R.id.topic_finish);
+        topicSelector = (Spinner) rootView.findViewById(R.id.categorySpinner);
+        databaseReference = FirebaseDatabase.getInstance().getReference(TOPICS_REFERENCE);
+        searchButtonR = (RelativeLayout)rootView.findViewById(R.id.start_searching_button);
+
+
+
 
 
         linearTimeAndDate = (LinearLayout) rootView.findViewById(R.id.linear_date_button);
@@ -248,9 +285,75 @@ public class CoffeOptionsFragment extends Fragment implements OnMapReadyCallback
             }
         });
 
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot categoriaSnap : dataSnapshot.getChildren()) {
+                    Topic topic = categoriaSnap.getValue(Topic.class);
+                    topic.setDataBaseReference(categoriaSnap.getKey());
+                    allTopics.add(topic);
+                }
+
+                LinkedList<String> list = new LinkedList<String>();
+                for(Topic topic:allTopics){
+                    list.add(topic.getDisplay_title());
+                }
+                ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(),
+                        android.R.layout.simple_spinner_item, list);
+                dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                topicSelector.setAdapter(dataAdapter);
+
+                categoryAdapter = new TopicsAdapter(allTopics,getActivity());
+                //categoryAdapter.setCategorySelectedListener(FilterTopicsActivity.this);
+                //     topicSelector.setAdapter(categoryAdapter);
+               // hidepDialog();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        topicSelector.setOnItemSelectedListener(new CoffeOptionsFragment.mySpinnerListener());
 
 
-        setUpLocationOptions();
+
+
+        searchButtonR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Gson gson = new Gson();
+                Bundle extras = new Bundle();
+                extras.putString(KEY_DATE,dateSelected);
+                extras.putString(KEY_TIME,timeSelected);
+                extras.putSerializable(KEY_CATEGORIA,categoriaSeleccionada);
+                if(ubicacionPreferida == null){
+                    extras.putString(KEY_FAVORITE_PLACE,"");
+
+                }else{
+                    extras.putString(KEY_FAVORITE_PLACE,gson.toJson(ubicacionPreferida));
+
+                }
+                extras.putDouble(KEY_CURRENT_LATIDU,latitud);
+                extras.putDouble(KEY_CURRENT_LONGITUD,longitud);
+                if(placeSeleccionado == null){
+                    extras.putString(KEY_PLACE_SELECTED,"");
+
+                }else{
+                    extras.putString(KEY_PLACE_SELECTED,gson.toJson(placeSeleccionado));
+
+                }
+                extras.putInt(KEY_PARTY_NUMBER,number);
+                extras.putSerializable(KEY_TOPIC,topicSeleccionado);
+                Intent filterIntent = new Intent(getActivity(),SearchActivity.class);
+                filterIntent.putExtras(extras);
+                startActivity(filterIntent);
+                getActivity().finish();
+            }
+        });
 
 
         return  rootView;
@@ -269,14 +372,22 @@ public class CoffeOptionsFragment extends Fragment implements OnMapReadyCallback
         isFirsttime = true;
         fakeOnCreate = true;
         justCoffe = true;
+        allTopics = new LinkedList<>();
+
     }
 
     public void updateLabel(String date){
+        dateSelected = date;
         dateEditText.setText(date);
+        optionsIsFinish();
+
     }
 
     public void updateTimeLabel(String data){
+        timeSelected = data;
         timeEditText.setText(data);
+        optionsIsFinish();
+
     }
 
     public void finishOption(int finishedOption){
@@ -295,7 +406,9 @@ public class CoffeOptionsFragment extends Fragment implements OnMapReadyCallback
 
 
     private void toogleLocation(){
-            if(isFirsttime){
+        optionsIsFinish();
+
+        if(isFirsttime){
                 if(onActionSelectedListener != null){
                     onActionSelectedListener.OnFirstOptionSelected();
                     isFirsttime = false ;
@@ -312,6 +425,8 @@ public class CoffeOptionsFragment extends Fragment implements OnMapReadyCallback
     }
 
     private void toggleFilters(){
+        optionsIsFinish();
+
         if(isFirsttime){
             if(onActionSelectedListener != null){
                 onActionSelectedListener.OnFirstOptionSelected();
@@ -329,6 +444,7 @@ public class CoffeOptionsFragment extends Fragment implements OnMapReadyCallback
     }
 
     private void toggleTimeAndDate(){
+        optionsIsFinish();
         if(isFirsttime){
             if(onActionSelectedListener != null){
                 onActionSelectedListener.OnFirstOptionSelected();
@@ -399,20 +515,20 @@ public class CoffeOptionsFragment extends Fragment implements OnMapReadyCallback
         searchButton = (ImageView) rootView.findViewById(R.id.search_button);
         centerButton = (ImageView)rootView.findViewById(R.id.center_button);
         myScroolView = (ScrollView)rootView.findViewById(R.id.myScroolView);
-        searchButton.setOnClickListener(new View.OnClickListener() {
+       /* searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 searchPlaces();
             }
-        });
+        });*/
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
         pDialog = new ProgressDialog(getActivity());
         pDialog.setMessage(getString(R.string.please_wait_d));
         pDialog.setCancelable(false);
         allcategorias = new LinkedList<>();
         historicPlaces = new LinkedList<>();
-        showpDialog();
-        justCoffe = false ;
+       // showpDialog();
+        justCoffe = true ;
         databaseReference = FirebaseDatabase.getInstance().getReference(CATEGORIAS_REFERENCE);
 
         mapFragment = (WorkaroundMapFragment) getChildFragmentManager()
@@ -452,6 +568,7 @@ public class CoffeOptionsFragment extends Fragment implements OnMapReadyCallback
                         public void onSuccess(Location location) {
                             if (location != null) {
                                 currentLocation = location;
+
                                 centerLocationOnScreenFirstTime();
                             }
                         }
@@ -611,6 +728,9 @@ public class CoffeOptionsFragment extends Fragment implements OnMapReadyCallback
                     if(plafes.getLatitude() == marker.getPosition().latitude ){
                         if(plafes.getLongitude() == marker.getPosition().longitude){
                             placeSeleccionado = plafes;
+                            locationFinish.setVisibility(View.VISIBLE);
+                            optionsIsFinish();
+
                             break;
                         }
                     }
@@ -785,6 +905,39 @@ public class CoffeOptionsFragment extends Fragment implements OnMapReadyCallback
         }
     }
 
+
+
+    class mySpinnerListener implements Spinner.OnItemSelectedListener
+    {
+        @Override
+        public void onItemSelected(AdapterView parent, View v, int position,
+                                   long id) {
+
+            Toast.makeText(parent.getContext(),
+                    "Topic selected : " + parent.getItemAtPosition(position).toString(),
+                    Toast.LENGTH_SHORT).show();
+            topicSeleccionado = allTopics.get(position);
+            topiFinish.setVisibility(View.VISIBLE);
+            optionsIsFinish();
+
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView parent) {
+            // TODO Auto-generated method stub
+            // Do nothing.
+        }
+
+    }
+
+
+
+    private void optionsIsFinish(){
+        if(dateSelected != null && dateSelected.length() >1 && timeSelected != null && timeSelected.length() >1 && placeSeleccionado != null){
+            searchButtonR.setVisibility(View.VISIBLE);
+        }
+    }
 
 
 
